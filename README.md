61f3b38d973e679217674e8365d93552caffed3f
joshbindels <josh.bindels@gmail.com>
2018-06-21 10:47:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Code Image Tool
+
+Taking screenshots of code can be really annoying, especially when
+the code has many lines. This tools attempts to solve this problem
+by creating an image of the code.
---
+++ constants.py
@@ -0,0 +1,19 @@
+BLACK   = (0,   0,   0  )
+WHITE   = (255, 255, 255)
+PURPLE  = (186, 53,  95)
+
+KEYWORDS = [
+    "#include",
+    "#define",
+    "void",
+    "void*",
+    "int",
+    "int*",
+    "char",
+    "char*",
+    "NULL",
+    "for",
+    "if",
+    "else",
+    "case"
+]
