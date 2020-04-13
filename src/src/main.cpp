2b1e74b0a4ca008beb703efe784d4c80d4d0b3a2
joshbindels <josh.bindels@gmail.com>
2020-04-13 22:44:23 +0200

    Add image creation

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -5,6 +5,8 @@
 #include <regex>
 #include "CONST.hpp"
 
+#include <opencv2/opencv.hpp>
+
 #define LOG(x) std::cout << x << std::endl;
 
 
@@ -16,6 +18,7 @@ enum TOKEN_TYPE
     WHITESPACE,
     ASSIGNMENT,
     KEYWORD,
+    NEWLINE,
     UNKNOWN
 };
 
@@ -36,6 +39,8 @@ std::string TranslateTokenTypeToString(TOKEN_TYPE& t)
             return "ASSIGNMENT";
         case TOKEN_TYPE::KEYWORD:
             return "KEYWORD";
+        case TOKEN_TYPE::NEWLINE:
+            return "NEWLINE";
         case TOKEN_TYPE::UNKNOWN:
             return "UNKNOWN";
         default:
@@ -48,6 +53,8 @@ class Token
 public:
     Token(TOKEN_TYPE type, std::string value):m_Type(type),m_Value(value){};
     ~Token(){};
+    std::string GetValue(){ return m_Value; }
+    TOKEN_TYPE GetType(){ return m_Type; }
     std::string ToString()
     {
         std::ostringstream ss;
@@ -68,6 +75,7 @@ private:
 
 std::string getFullString(std::string& fullStr, std::string quoteChar, int& i)
 {
+    std::cout << "getFullString" << std::endl;
     std::string res = quoteChar;
     i++;
     while(fullStr[i] != quoteChar[0])
@@ -85,22 +93,30 @@ std::string getFullRegex(std::string& fullStr, int& i, std::string re)
     {
         res += fullStr[i++];
     }
+    i--;
     return res;
 }
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
     std::vector<Token> lex;
+    std::string TEST_STRING = "int main(int argc, char** argv)\n{\n    int a = 123;\n}";
+    std::string symbols = "!%^&*_+/?.,<>|~-=(){}[]";
 
+    std::cout << "String length: " << TEST_STRING.length() << std::endl;
+    LOG(TEST_STRING);
     for(int i=0; i<TEST_STRING.length(); i++)
     {
-        std::string symbols = "!%^&*()_+/?.,<>|`~-={}[]";
-
-        if(TEST_STRING[i] == ' ' || TEST_STRING[i] == '\n')
+        if(TEST_STRING[i] == ' ')
         {
             lex.push_back(Token(TOKEN_TYPE::WHITESPACE, std::string(1, TEST_STRING[i])));
         }
-        else if(symbols.find(TEST_STRING[i]) != std::string::npos)
+        else if(TEST_STRING[i] == '\n')
+        {
+            lex.push_back(Token(TOKEN_TYPE::NEWLINE, std::string(1, TEST_STRING[i])));
+        }
+        //else if(symbols.find(TEST_STRING[i]) != std::string::npos)
+        else if(std::ispunct(TEST_STRING[i]))
         {
             lex.push_back(Token(TOKEN_TYPE::SYMBOL, std::string(1, TEST_STRING[i])));
         }
@@ -124,12 +140,29 @@ int main(int, char**)
             lex.push_back(Token(TOKEN_TYPE::UNKNOWN, std::string(1, TEST_STRING[i])));
         }
 
-
     }
 
+
+    cv::Mat image(800, 800, CV_8UC3, cv::Scalar(255, 255, 255));
+
+    int x = 0;
+    int y = 20;
     for(Token &l: lex)
     {
+        if (l.GetType() == TOKEN_TYPE::NEWLINE)
+        {
+            y+=20;
+            x = 0;
+        }
+        else
+        {
+            cv::putText(image, l.GetValue(), cv::Point(x,y), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0,0,0));
+            x += 10 * l.GetValue().length();
+        }
         LOG(l.ToString());
     }
+
+    imwrite("outputimage.jpg", image);
+
     return 0;
 }
